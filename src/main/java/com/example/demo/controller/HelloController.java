1174aa94a877e0c158ecbd7b519f852dc6fbf5c1
TAEWOOKK <kto5294@gmail.com>
2024-01-21 21:11:25 +0900

    develop-BE-woook tmdb 영화 불러오기 셋팅

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,37 +1,39 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.CRDTService;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/crdt")
 @RequiredArgsConstructor
 public class HelloController {
 
-    //develop_BE-wook 생성
-    private final CRDTService crdtService;
-
-    private final SimpMessagingTemplate messagingTemplate;
-
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/account/20907790/lists?page=1")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTdkZDg2MGJkYzJmNzAwNDI2NjcwNmQ4ZGJhYzI1NSIsInN1YiI6IjY1OWJlMzI3YmQ1ODhiMjA5OThkNDI3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TydEZPf9nrIucJSP8WIfQszoJzX9hXJXv2nNTaTIJo4")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String responseBody = response.body().string();
+
+        model.addAttribute("response", responseBody);
+
         return "index";
     }
-    @ResponseBody
-    @PostMapping("/increment")
-    public void increment(@RequestParam String key) {
-        crdtService.increment(key);
-        messagingTemplate.convertAndSend("/topic/crdt-updates", crdtService.getCRDTMap());
-    }
 
-    @ResponseBody
-    @GetMapping("/get")
-    public Map<String, Integer> getCRDTMap() {
-        return crdtService.getCRDTMap();
-    }
 }
