c39ff5c9ce0cff213af27d0ed2989bd5f748c4c8
TAEWOOKK <kto5294@gmail.com>
2024-01-21 21:13:59 +0900

    develop-BE-woook tmdb 영화 불러오기 셋팅

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Controller
 @RequiredArgsConstructor
 public class HelloController {
-
+//
     @GetMapping("/")
     public String index(Model model) throws IOException {
 
