ec1f8354585743d53ccf2464e0a43db271963e13
TAEWOOKK <kto5294@gmail.com>
2024-01-29 00:07:52 +0900

    현재 상영중 리스트 출력

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -60,4 +60,45 @@ public class HelloController {
 
         return "index";
     }
+
+    @GetMapping("/nowPlaying")
+    public String list1 (Model model) throws IOException {
+        //1a4fdfbb72ca9489d8eb9487d7a4ccff4434ec32
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTdkZDg2MGJkYzJmNzAwNDI2NjcwNmQ4ZGJhYzI1NSIsInN1YiI6IjY1OWJlMzI3YmQ1ODhiMjA5OThkNDI3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TydEZPf9nrIucJSP8WIfQszoJzX9hXJXv2nNTaTIJo4")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String jsonData = response.body().string();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String,Object>> movie = new ArrayList<>();
+
+        JsonNode rootNode = objectMapper.readTree(jsonData);
+        JsonNode resultsNode = rootNode.get("results");
+
+        if (resultsNode.isArray()) {
+            for (JsonNode movieNode : resultsNode) {
+                String title = movieNode.get("title").asText();
+                String overview = movieNode.get("overview").asText();
+                String poster_path = movieNode.get("poster_path").asText();
+                Map<String,Object> map = new HashMap<>();
+
+                map.put("title",title);
+                map.put("overview",overview);
+                map.put("poster_path","https://image.tmdb.org/t/p/w500"+poster_path);
+
+                movie.add(map);
+            }
+        }
+        model.addAttribute("movie", movie);
+
+        return "list1";
+    }
 }
