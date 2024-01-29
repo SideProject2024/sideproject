a36d36e098d6da11808cb9f51d0fe891b3669606
TAEWOOKK <kto5294@gmail.com>
2024-01-29 23:32:26 +0900

    디테일 페이지 구현

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,9 +6,11 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -46,10 +48,12 @@ public class HelloController {
             for (JsonNode movieNode : resultsNode) {
                 String title = movieNode.get("title").asText();
                 String overview = movieNode.get("overview").asText();
+                String id = movieNode.get("id").asText();
                 String poster_path = movieNode.get("poster_path").asText();
                 Map<String,Object> map = new HashMap<>();
 
                 map.put("title",title);
+                map.put("id",id);
                 map.put("overview",overview);
                 map.put("poster_path","https://image.tmdb.org/t/p/w500"+poster_path);
 
@@ -86,11 +90,13 @@ public class HelloController {
         if (resultsNode.isArray()) {
             for (JsonNode movieNode : resultsNode) {
                 String title = movieNode.get("title").asText();
+                String id = movieNode.get("id").asText();
                 String overview = movieNode.get("overview").asText();
                 String poster_path = movieNode.get("poster_path").asText();
                 Map<String,Object> map = new HashMap<>();
 
                 map.put("title",title);
+                map.put("id",id);
                 map.put("overview",overview);
                 map.put("poster_path","https://image.tmdb.org/t/p/w500"+poster_path);
 
@@ -100,5 +106,53 @@ public class HelloController {
         model.addAttribute("movie", movie);
 
         return "list1";
+    }
+
+    @GetMapping("/content/{movie_id}")
+    public String movieDetail(@PathVariable("movie_id")String movie_id,Model model) throws IOException {
+
+        //1a4fdfbb72ca9489d8eb9487d7a4ccff4434ec32
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/"+movie_id+"?language=ko-KR")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTdkZDg2MGJkYzJmNzAwNDI2NjcwNmQ4ZGJhYzI1NSIsInN1YiI6IjY1OWJlMzI3YmQ1ODhiMjA5OThkNDI3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TydEZPf9nrIucJSP8WIfQszoJzX9hXJXv2nNTaTIJo4")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String jsonData = response.body().string();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String,Object>> movie = new ArrayList<>();
+
+        JsonNode result = objectMapper.readTree(jsonData);
+
+        String backdrop_path = String.valueOf(result.get("backdrop_path"));
+
+        backdrop_path = backdrop_path.replaceAll("\"", "");
+
+        JsonNode belongsToCollection = result.get("belongs_to_collection");
+
+        String name = belongsToCollection.get("name").asText();
+        String posterPath = belongsToCollection.get("poster_path").asText();
+        String backdropPath = belongsToCollection.get("backdrop_path").asText();
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("name",name);
+        map.put("posterPath",backdropPath);
+        map.put("backdropPath",backdropPath);
+        map.put("backdrop_path",backdrop_path);
+
+
+
+        model.addAttribute("backdrop_path", "https://image.tmdb.org/t/p/w500"+backdrop_path);
+        model.addAttribute("movie",map);
+        model.addAttribute("result", result);
+
+        return "detail";
     }
 }
