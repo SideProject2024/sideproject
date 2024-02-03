9d631f7f8ef0f2debef9c7cb0ecd228995413b4c
TAEWOOKK <kto5294@gmail.com>
2024-02-04 00:48:42 +0900

    메인 컨텐츠 서비스 클래스 생성

---
+++ src/main/java/com/example/demo/service/Total_mainService.java
@@ -0,0 +1,93 @@
+package com.example.demo.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+public class Total_mainService {
+
+    public Map<String,Object> MovieDetail(String movie_id) throws IOException {
+
+        //1a4fdfbb72ca9489d8eb9487d7a4ccff4434ec32
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/"+movie_id+"?language=ko-KR")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTdkZDg2MGJkYzJmNzAwNDI2NjcwNmQ4ZGJhYzI1NSIsInN1YiI6IjY1OWJlMzI3YmQ1ODhiMjA5OThkNDI3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TydEZPf9nrIucJSP8WIfQszoJzX9hXJXv2nNTaTIJo4")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String jsonData = response.body().string();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String,Object>> movie = new ArrayList<>();
+
+        JsonNode result = objectMapper.readTree(jsonData);
+
+
+        String poster_path = result.get("poster_path").asText();
+        String backdrop_path = result.get("backdrop_path").asText();
+        String title = result.get("title").asText();
+        String tagline = result.get("tagline").asText();
+        String overview = result.get("overview").asText();
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("poster_path","https://image.tmdb.org/t/p/w500"+poster_path);
+        map.put("backdrop_path","https://image.tmdb.org/t/p/original"+backdrop_path);
+        map.put("title",title);
+        map.put("tagline",tagline);
+        map.put("overview",overview);
+
+        return map;
+    }
+
+    public Map<String,Object> MovieCredits(String movie_id) throws IOException {
+        //1a4fdfbb72ca9489d8eb9487d7a4ccff4434ec32
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/"+movie_id+"?language=ko-KR")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTdkZDg2MGJkYzJmNzAwNDI2NjcwNmQ4ZGJhYzI1NSIsInN1YiI6IjY1OWJlMzI3YmQ1ODhiMjA5OThkNDI3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TydEZPf9nrIucJSP8WIfQszoJzX9hXJXv2nNTaTIJo4")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String jsonData = response.body().string();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String,Object>> movie = new ArrayList<>();
+
+        JsonNode result = objectMapper.readTree(jsonData);
+
+
+        String poster_path = result.get("poster_path").asText();
+        String backdrop_path = result.get("backdrop_path").asText();
+        String title = result.get("title").asText();
+        String tagline = result.get("tagline").asText();
+        String overview = result.get("overview").asText();
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("poster_path","https://image.tmdb.org/t/p/w500"+poster_path);
+        map.put("backdrop_path","https://image.tmdb.org/t/p/original"+backdrop_path);
+        map.put("title",title);
+        map.put("tagline",tagline);
+        map.put("overview",overview);
+
+        return map;
+    }
+}
