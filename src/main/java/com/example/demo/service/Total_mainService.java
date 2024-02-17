8bddb5c58b5ab22e1347719a3ccc53b2285e627c
TAEWOOKK <kto5294@gmail.com>
2024-02-17 23:05:15 +0900

    영화 한국어 번역

--- src/main/java/com/example/demo/service/Total_mainService.java
+++ src/main/java/com/example/demo/service/Total_mainService.java
@@ -8,7 +8,11 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.*;
 
 @Service
@@ -75,8 +79,8 @@ public class Total_mainService {
 
         if (resultsNode.isArray()) {
             for (JsonNode movieNode : resultsNode) {
-                String name = movieNode.get("name").asText();
-                String character = movieNode.get("character").asText();
+                String name = EntoKo(movieNode.get("name").asText());
+                String character = EntoKo(movieNode.get("character").asText());
                 String profile_path = movieNode.get("profile_path").asText();
                 Map<String,Object> map = new HashMap<>();
 
@@ -89,5 +93,94 @@ public class Total_mainService {
         }
 
         return actors;
+    }
+
+    public String EntoKo(String text){
+        String clientId = "h_WrQMXqqQ3vNQvmdOCr";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "gS8vECFbLi";//애플리케이션 클라이언트 시크릿값";
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        try {
+            text = URLEncoder.encode(text, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = post(apiURL, requestHeaders, text);
+
+        String translatedText = "";
+        try {
+            // Parse JSON string
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+            // Extract the value of the "translatedText" field
+            translatedText = jsonNode.path("message").path("result").path("translatedText").asText();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return translatedText;
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
     }
 }
