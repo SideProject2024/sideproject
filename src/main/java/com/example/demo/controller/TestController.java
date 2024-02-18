8b93768186e305288e3739796f36e25a3b77ba99
Lianiscoming <121073208+Lianiscoming@users.noreply.github.com>
2024-02-18 21:44:30 +0900

    for sang_1

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test/users")
+@RequiredArgsConstructor
+public class TestController {
+        @GetMapping("/member/{name}")
+        public String findByName(Model model)
+            public String findByName(@PathVariable("name") String name) {
+            return "Name: " + name;
+        }
+
+        @GetMapping("member/{id}/{name}")
+        public String findByNameAndId(@PathVariable("id") String id,
+                                  @PathVariable("name") String name) {
+            return "ID: " + id + ", name: " + name;
+        }
+
+}
